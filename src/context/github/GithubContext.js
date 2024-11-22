4b2c2b8adfe267a634f42804034a9a5d0b6896c1
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2024-11-22 14:43:57 +0300

    Completed moving searchUsers to Action file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -20,28 +20,6 @@ export const GithubProvider = ({ children }) => {
   // Clear users from state
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
-  //   Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}
-  `,
-      },
-    });
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
   // Get single user
   const getUser = async (login) => {
     setLoading();
@@ -99,11 +77,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
